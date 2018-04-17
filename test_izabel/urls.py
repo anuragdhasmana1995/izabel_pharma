43b5748b9d645f67ba573c8a425cbfd568dff57b
Anurag Dhasmana <anuragdhasmana1995@gmail.com>
2018-04-18 03:02:28 +0530

    static in root dir

--- test_izabel/urls.py
+++ test_izabel/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^medicines/', include('medicines.urls')),
+    url(r'^', include('medicines.urls')),
 ]
 
 if settings.DEBUG:
